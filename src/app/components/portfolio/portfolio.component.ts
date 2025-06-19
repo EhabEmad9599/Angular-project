dfb032560bb6d0e2814246c1fc0e5e9bb32600ed
Ehab Emad <47368484+EhabEmad9599@users.noreply.github.com>
2025-06-19 05:03:43 +0300

    Add files via upload

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
 
+
+interface Cards  {
+  id:number,
+  imgUrl:string
+
+}
+
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
+
+
+
 export class PortfolioComponent {
+  
+    cards: Cards[] = [
+    {
+      id: 1,
+      imgUrl:
+        'assets/poert1.png',
+    },
+    {
+      id: 2,
+      imgUrl:
+        'assets/port2.png',
+    },
+    {
+      id: 3,
+      imgUrl:
+        'assets/port3.png',
+    },
+    {
+      id: 4,
+      imgUrl:
+        'assets/poert1.png',
+    },
+    {
+      id: 5,
+      imgUrl:
+        'assets/port2.png',
+    },
+    {
+      id: 6,
+      imgUrl:
+        'assets/port3.png',
+    },
+  ];
 
 }
